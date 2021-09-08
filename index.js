bc56fb5a75215163cf7ec3f2f654c32ed7c8e186
Olga Panfilova <gr-olga@ya.ru>
2021-09-08 13:30:49 +0200

    made find sum

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ const cardsList = [
         place: "MoonMarket",
         sum: "600",
         period: "11/11/12"
+    },
+    {
+        place: "SunMarket",
+        sum: "200",
+        period: "12/12/12"
+    },
+    {
+        place: "MoonMarket",
+        sum: "600",
+        period: "11/11/12"
     }
 ]
 
@@ -24,12 +34,40 @@ cardsList.forEach((obj) => renderList(obj.place, obj.sum, obj.period));
 function renderList(place, sum, period) {
     const listGrid = document.querySelector('.result');
     const listTemplate = document.querySelector('#list').content;
-     const listElement = listTemplate.querySelector('.container').cloneNode(true);
+    const listElement = listTemplate.querySelector('.container').cloneNode(true);
     listElement.querySelector('.place').textContent = place;
     listElement.querySelector('.sum').textContent = sum;
     listElement.querySelector('.period').textContent = period;
     listGrid.prepend(listElement);
 }
 
-submitBtn.addEventListener('click', renderList);
+// submitBtn.addEventListener('click', () => countSum(cardsList));
 
+
+function countSum(list) {
+    const sumSun = list
+        .map((item) => item.sum)
+        .map(Number)
+        .reduce((akk, prev) => akk + prev, 0)
+    console.log(sumSun)
+}
+
+submitBtn.addEventListener('click', () => placeName(cardsList));
+
+function sumByPlace(list, placeName) {
+    const sumSun = list
+        .filter((item) => item.place === placeName)
+        .map((item) => item.sum)
+        .map(Number)
+        .reduce((akk, prev) => akk + prev, 0)
+    return sumSun
+}
+
+function placeName(list) {
+    const res = {}
+    list.map((item) => item.place)
+        .forEach((place) => {
+            res[place] = sumByPlace(list, place);
+        })
+    console.log(res)
+}
