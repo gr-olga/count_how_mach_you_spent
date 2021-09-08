24185a748862415c9973c83ea63205856ac51f92
Olga Panfilova <gr-olga@ya.ru>
2021-09-08 12:24:37 +0200

    initial commit

---
+++ components/List.js
@@ -0,0 +1,22 @@
+// export class List{
+//     constructor(place, sum, period) {
+//         this._place = place;
+//         this._sum = sum;
+//         this._period = period;
+//     }
+//
+//     _getTemplate() {
+//         this._listTemplate = document.querySelector('#list').content;
+//         return this._listTemplate.querySelector('.result-grid').cloneNode(true)
+//     }
+//     generateItem(){
+//         this._element = this._getTemplate();
+//         this._element.querySelector('.place').textContent = this._place;
+//         this._element.querySelector('.sum').textContent = this._sum;
+//         this._element.querySelector('.period').textContent = this._period;
+//     }
+//
+// }
+
+
+
